7963d6608e468900d7edd442b2c913bcb12cf96f
Sonnie Nguyen <sonnienguyen@mbpro-intsonnie.ssi.com>
2025-07-23 23:04:01 -0500

    stuff

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Component, inject, signal } from '@angular/core';
+import { IonButton, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { GeminiService } from '../services/gemini-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    IonButton
+  ],
 })
 export class HomePage {
   public currDay = signal<number>(30);
+  public story = signal<string | undefined>('');
+  private readonly gemini = inject(GeminiService);
 
+  async advanceStory() {
+    this.story.set(await this.gemini.main());
+  }
 }
--- src/app/services/gemini-service.service.ts
+++ src/app/services/gemini-service.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@angular/core';
 import { GoogleGenAI } from "@google/genai";
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
-export class GeminiServiceService {
-  private readonly ai = new GoogleGenAI({});
-  
+export class GeminiService {
+  private readonly ai = new GoogleGenAI({ apiKey: environment.apiKey});
 
+  async main(): Promise<string | undefined> {
+  try {
+      const response = await this.ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: "Explain how AI works in a few words",
+      });
+    return response.text;
+  } catch (err) {
+    console.error(err);
+    return '';
+  }
+  }
 }
