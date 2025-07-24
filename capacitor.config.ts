9d2f3eaa8b06ec1054430cbb74da38eb219541a1
Sonnie Nguyen <sonnienguyen@mbpro-intsonnie.ssi.com>
2025-07-23 21:40:55 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'In30Days',
+  webDir: 'www'
+};
+
+export default config;
